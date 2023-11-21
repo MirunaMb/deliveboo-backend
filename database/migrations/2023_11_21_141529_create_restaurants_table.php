6c052fa15a7ec9966ae32ad15988ed9c7cc51d18
JordiMasci <133970995+JordiMasci@users.noreply.github.com>
2023-11-21 20:09:20 +0100

    feat: add user_id

--- database/migrations/2023_11_21_141529_create_restaurants_table.php
+++ database/migrations/2023_11_21_141529_create_restaurants_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -15,6 +14,7 @@ return new class extends Migration
     {
         Schema::create('restaurants', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained(); // Assicurati di avere 'users' come nome della tabella degli utenti
             $table->string('name');
             $table->text('description');
             $table->string('address');
@@ -23,6 +23,7 @@ return new class extends Migration
             $table->string('image')->nullable();
             $table->timestamps();
         });
+
     }
 
     /**
