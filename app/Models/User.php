2b91155e03353c7c8f691adca9b05845d4b0e46f
JordiMasci <133970995+JordiMasci@users.noreply.github.com>
2023-11-21 17:40:11 +0100

    feat: add one to one (user-restaurant)

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    // Collegamento con Restaurant
+    public function restaurant()
+    {
+        return $this->hasOne(Restaurant::class);
+    }
 }
