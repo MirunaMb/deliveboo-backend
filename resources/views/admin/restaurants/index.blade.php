ec0465beed6c253a2efe64cf8926b29725273500
JordiMasci <133970995+JordiMasci@users.noreply.github.com>
2023-11-22 11:05:52 +0100

    feat: add fontawesome/ button edit

--- resources/views/admin/restaurants/index.blade.php
+++ resources/views/admin/restaurants/index.blade.php
@@ -1,5 +1,11 @@
 @extends('layouts.app')
 
+@section('font-awesome')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+        integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+@endsection
+
 @section('content')
     <div class="container">
         <table class="table">
@@ -10,30 +16,31 @@
                     <th scope="col">Address</th>
                     <th scope="col">Phone Number</th>
                     <th scope="col">Vat</th>
+                    <th scope="col">Modifica</th>
                 </tr>
             </thead>
             <tbody>
 
-                
+
                 {{-- @forelse ($restaurants as $restaurant) --}}
-                    <tr>
-                        <th scope="row">{{ $restaurant->id }}</th>
-                        <td>{{ $restaurant->name }}</td>
-                        <td>{{ $restaurant->address}}</td>
-                        <td>{{ $restaurant->phone_number }}</td>
-                        <td>{{ $restaurant->vat }}</td> 
+                <tr>
+                    <th scope="row">{{ $restaurant->id }}</th>
+                    <td>{{ $restaurant->name }}</td>
+                    <td>{{ $restaurant->address }}</td>
+                    <td>{{ $restaurant->phone_number }}</td>
+                    <td>{{ $restaurant->vat }}</td>
+                    <td>
+                        <a href="{{ route('admin.restaurant.edit', $restaurant) }}" class="mx-2">
+                            <i class="fa-solid fa-pen-to-square"></i>
+                        </a>
+                    </td>
 
-
-                        {{-- <td>
+                    {{-- <td>
                             <a href="{{ route('admin.projects.show', $project) }}" class="mx-2">
                                 <i class="fa-solid fa-eye"></i>
                             </a>
                         </td>
-                        <td>
-                            <a href="{{ route('admin.projects.edit', $project) }}" class="mx-2">
-                                <i class="fa-solid fa-pen-to-square"></i>
-                            </a>
-                        </td>
+                        
                         <td>
                             <a href="javascript:void(0)" class="mx-2 text-danger" data-bs-toggle="modal"
                                 data-bs-target="#deleteModal-{{ $project->id }}">
@@ -41,12 +48,14 @@
                                 fa-trash"></i>
                             </a>
                         </td>  --}}
-                    </tr>
+                </tr>
 
-                 {{-- @empty
+                {{-- @empty
                     <td colspan="6"><i>Non ci sono risultati</i></td> --}}
-               {{-- @endforelse --}}
+                {{-- @endforelse --}}
             </tbody>
         </table>
+
+        
     </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-  <head>
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -15,18 +15,20 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
+    @yield('font-awesome')
+
     <!-- Usando Vite -->
     @vite(['resources/js/app.js'])
-  </head>
+</head>
 
-  <body>
+<body>
     <div id="app">
-      @include('partials._navbar')
+        @include('partials._navbar')
 
-      <main class="">
-        @yield('content')
-      </main>
+        <main class="">
+            @yield('content')
+        </main>
     </div>
-  </body>
+</body>
 
 </html>
