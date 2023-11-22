59272e13cad9a3107582083d6fa115a935cfa30d
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2023-11-22 19:13:20 +0100

    FEAT: add types many to many

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,7 +18,9 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
+            TypeSeeder::class,
             RestaurantSeeder::class,
+            RestaurantTypeSeeder::class,
         ]);
     }
 }
