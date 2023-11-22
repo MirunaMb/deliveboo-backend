3287ce1aedb9fbc6128bd28c3bfd5ef0dabfa642
JordiMasci <133970995+JordiMasci@users.noreply.github.com>
2023-11-22 12:44:30 +0100

    feat: finish validator(manca img)

--- app/Http/Controllers/Admin/RestaurantController.php
+++ app/Http/Controllers/Admin/RestaurantController.php
@@ -128,11 +128,33 @@ class RestaurantController extends Controller
                 'address' => 'required|string|max:50',
                 'phone_number' => 'required|string|max:50',
                 'vat' => 'required|string|max:50',
-                // 'image' => 'nullable|image|max:1024',
                 'description' => 'required',
+                // 'image' => 'nullable|image|max:1024',
             ],
             [
-                'name.required' => 'name is binding',
+                'name.required' => 'Il nome è obbligatorio',
+                'name.string' => 'Il nome deve essere una stringa',
+                'name.max' => 'Il nome deve contenere un massimo di 50 caratteri',
+
+                'address.required' => 'L\'indirizzo è obbligatorio',
+                'address.string' => 'L\'indirizzo deve essere una stringa',
+                'address.max' => 'L\'indirizzo deve contenere un massimo di 50 caratteri',
+
+                'phone_number.required' => 'Il numero di telefono è obbligatorio',
+                'phone_number.string' => 'Il numero di telefono è una stringa',
+                'phone_number.max' => 'Il numero di telefono deve contenere un massimo di 50 caratteri',
+
+                'vat.required' => 'La partita IVA è obbligatorio',
+                'vat.string' => 'La partita IVA è una stringa',
+                'vat.max' => 'La partita IVA deve contenere un massimo di 50 caratteri',
+                
+                'description.required' => 'La partita IVA è obbligatorio',
+                
+                // DA GESTIRE QUANDO INSERIAMO IMAGE
+                // 'image.image' => 'L\'immagine',
+                // 'image.max' => 'La partita IVA deve contenere un massimo di 50 caratteri',
+
+                
             ]
         )->validate();
         return $validator;
