aeffba704c1e63ddd55c2b32907fe1a066a0d37a
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2023-11-23 10:27:28 +0100

    feat: add DishSeeder contents

--- database/seeders/DishSeeder.php
+++ database/seeders/DishSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use App\Models\Dish;
+
 class DishSeeder extends Seeder
 {
     /**
@@ -14,28 +16,23 @@ class DishSeeder extends Seeder
      */
     public function run()
     {
-        // $_restaurants = [
-        //     ['name' => 'ristorante_1', 'description' => 'Linguaggio di markup strutturale.', 'address' => 'via vatte a pesca', 'phone_number' => '333333333333', 'vat' => '86334519757', 'image' => '', 'user_id' => 1],
-        //     ['name' => 'ristorante_2', 'description' => 'Foglio di stile per presentazione.', 'address' => 'via vatte a pesca', 'phone_number' => '333333333333', 'vat' => '96334519757', 'image' => '', 'user_id' => 2],
-        //     ['name' => 'ristorante_3', 'description' => 'Linguaggio di scripting client-side.', 'address' => 'via vatte a pesca', 'phone_number' => '333333333333', 'vat' => '76334519757', 'image' => '', 'user_id' => 3],
-        //     ['name' => 'ristorante_4', 'description' => 'Framework JavaScript per interfacce utente.', 'address' => 'via vatte a pesca', 'phone_number' => '333333333333', 'vat' => '66334519757', 'image' => '', 'user_id' => 4],
-        //     ['name' => 'ristorante_5', 'description' => 'Linguaggio di scripting server-side.', 'address' => 'via vatte a pesca', 'phone_number' => '333333333333', 'vat' => '56334519757', 'image' => '', 'user_id' => 5],
-        // ];
+        $_dishes = [
+            ['name' => 'ristorante_1', 'description' => 'Linguaggio di markup strutturale.', 'visible' => true, 'image' => '', 'price' => 12, 40],
+            ['name' => 'ristorante_2', 'description' => 'Foglio di stile per presentazione.', 'visible' => true, 'image' => '', 'price' => 20, 21],
+            ['name' => 'ristorante_3', 'description' => 'Linguaggio di scripting client-side.', 'visible' => true, 'image' => '', 'price' => 30, 00],
+            ['name' => 'ristorante_4', 'description' => 'Framework JavaScript per interfacce utente.', 'visible' => true, 'image' => '', 'price' => 12, 93],
+            ['name' => 'ristorante_5', 'description' => 'Linguaggio di scripting server-side.', 'visible' => true, 'image' => '', 'price' => 7, 50],
+        ];
 
 
 
-        // foreach ($_restaurants as $_restaurant) {
-        //     $restaurant = new Restaurant();
-        //     $restaurant->name = $_restaurant['name'];
-        //     $restaurant->description = $_restaurant['description'];
-        //     $restaurant->address = $_restaurant['address'];
-        //     $restaurant->phone_number = $_restaurant['phone_number'];
-        //     $restaurant->vat = $_restaurant['vat'];
-        //     $restaurant->image = $_restaurant['image'];
-
-        //     $user = User::find($_restaurant['user_id']);
-        //     $user->restaurant()->save($restaurant);
-        // }
-
+        foreach ($_dishes as $_dish) {
+            $dish = new Dish();
+            $dish->name = $_dish['name'];
+            $dish->description = $_dish['description'];
+            $dish->visible = $_dish['visible'];
+            $dish->image = $_dish['image'];
+            $dish->price = $_dish['price'];
+        }
     }
 }
