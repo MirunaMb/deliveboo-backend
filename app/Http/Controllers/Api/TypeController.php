2b82bac9ed5e58df3fbafa6164491b03903ea1ae
luigitarallo <tarallo.gigi@gmail.com>
2023-11-24 19:08:57 +0100

    feat: add typecontroller

---
+++ app/Http/Controllers/Api/TypeController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Type;
+
+class TypeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $types = Type::select('id', 'label')->get();
+
+        return response()->json($types);
+    }
+
+    public function show($id)
+    {
+        $type = Type::select('id', 'label')
+            ->where('id', $id)
+            ->first();
+
+        return response()->json($type);
+    }
+
+}
