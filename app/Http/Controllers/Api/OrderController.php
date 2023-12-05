9e71aa069dc8840002a13ae2e0d526fbf7a6b56d
luigitarallo <tarallo.gigi@gmail.com>
2023-12-06 00:26:17 +0100

    feat: add function to ordercontroller

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\Order;
+use Braintree\Gateway;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Orders\OrderRequest;
 
 class OrderController extends Controller
 {
@@ -12,6 +14,7 @@ class OrderController extends Controller
     // Funzione che permette di ricevere i dati e salvarli nel database
     public function GetOrder(Request $request)
     {
+
         $request->validate([
             'guest_name' => 'required|string',
             'guest_surname' => 'required|string',
@@ -32,4 +35,39 @@ class OrderController extends Controller
 
         return response()->json(['message' => 'Ordine ricevuto con successo', 'order_id' => $order->id], 201);
     }
+    public function Generate(Request $request, Gateway $gateway)
+    {
+        $token = $gateway->clientToken()->generate();
+        $data = [
+            'success' => true,
+            'token' => $token
+        ];
+        return response()->json($data, 200);
+    }
+    public function MakePayment(OrderRequest $request, Gateway $gateway, Order $order)
+    {
+        dd($order);
+        $result = $gateway->transaction()->sale([
+            'amount' => 10,
+            'paymentMethodNonce' => $request->token,
+            'options' => [
+                'submitForSettlement' => true
+            ]
+        ]);
+        if ($result->success) {
+            $data = [
+                'message' => 'Transazione eseguita correttamente',
+                'success' => true
+            ];
+            return response()->json($data, 200);
+        } else {
+            $data = [
+                'message' => 'Transazione rifiutata',
+                'success' => false
+            ];
+            return response()->json($data, 401);
+        }
+    }
+
+
 }
