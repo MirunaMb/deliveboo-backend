a2a802b5a809df6313701c4cca59c32da63b7405
JordiMasci <133970995+JordiMasci@users.noreply.github.com>
2023-12-07 00:43:17 +0100

    feat: add layout for test

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-    
+    <div class="container">
+        @foreach ($orders as $order)
+        <div>{{$order->guest_name}}</div>   
+        @endforeach
+    </div>
 @endsection
