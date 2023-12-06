7db9d1b2b93c1c35e4bd668db3204ded2cc63c15
JordiMasci <133970995+JordiMasci@users.noreply.github.com>
2023-12-07 00:43:57 +0100

    feat: add index for OrderController

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
+use App\Models\Order;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class OrderController extends Controller
 {
@@ -13,8 +15,17 @@ class OrderController extends Controller
      */
     public function index()
     {
-        //
+        // Ottieni l'ID del ristorante associato all'utente autenticato
+        $restaurantId = Auth::user()->restaurant->id;
+
+        // Ottieni gli ordini relativi a quel ristorante
+        $orders = Order::whereHas('dishes.restaurant', function ($query) use ($restaurantId) {
+            $query->where('id', $restaurantId);
+        })->get();
+
+        return view('admin.orders.index', compact('orders'));
     }
+
 
     /**
      * Store a newly created resource in storage.
