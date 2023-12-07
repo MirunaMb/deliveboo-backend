1706a775d44e95b628a9ce9fa5dd42a939cbd0fe
JordiMasci <133970995+JordiMasci@users.noreply.github.com>
2023-12-07 18:33:49 +0100

    fix import class restaurant

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Models\Order;
-use App\Models\Restaurant;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
