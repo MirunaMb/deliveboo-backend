cdde7e63b7575a6ab3283c5efdb05ac26c1c153a
luigitarallo <tarallo.gigi@gmail.com>
2023-12-08 12:12:32 +0100

    feat: add show dishes in orders

--- resources/views/admin/orders/show.blade.php
+++ resources/views/admin/orders/show.blade.php
@@ -20,18 +20,31 @@
             </a>
         </div>
         {{-- * Order CARD --}}
-        <div class="card" style="width: 20rem;">
+        <div class="card">
             <div class="card-body">
                 <h5 class="card-title">Ordine</h5>
             </div>
             <ul class="list-group list-group-flush">
-                <li class="list-group-item"><strong>Nome: </strong>{{ $order->guest_name }}</li>
-                <li class="list-group-item"><strong>Cognome: </strong>{{ $order->guest_surname }}</li>
+                <li class="list-group-item">
+                    <span class="me-2">Nome: {{ $order->guest_name }}</span>
+                    <span class="me-2" >Cognome: {{ $order->guest_surname }}</span>
+                    <span class="me-2">Telefono: {{ $order->guest_phone }}</span>
+                    <span class="me-2">E-Mail:{{ $order->guest_mail }} </span>
+                </li>
                 <li class="list-group-item"><strong>Indirizzo: </strong>{{ $order->guest_address }}</li>
-                <li class="list-group-item"><strong>Telefono: </strong>{{ $order->guest_phone }}</li>
+                <li class="list-group-item"></li>
                 <li class="list-group-item"><strong>E-Mail: </strong>{{ $order->guest_mail }}</li>
                 <li class="list-group-item"><strong>Importo: </strong>€ {{ $order->totalItem }}</li>
-                <li class="list-group-item"><strong>Creato il: </strong>{{ $order->created_at }}</li>
+                <li class="list-group-item"><strong>Creato il: </strong>{{ $order->created_at->formatLocalized('%e/%m/%Y') }}</li>
+                <ul>
+                    @foreach ($dishes as $dish)
+            <li>{{ $dish->name }} - Quantità: {{ $dish->pivot->quantity }}
+                <img src="{{ $dish->image }}" alt="">
+            </li>
+        @endforeach
+                </ul>
+                    
+              
             </ul>
         </div>
     </div>
