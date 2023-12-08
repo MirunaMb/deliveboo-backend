10479685923c68347f2f0bd6003d1a4da5db6141
LauraDem <laura93dem@gmail.com>
2023-12-08 22:00:22 +0100

    style

--- resources/views/admin/dishes/show.blade.php
+++ resources/views/admin/dishes/show.blade.php
@@ -1,22 +1,28 @@
 @extends('layouts.app')
 
+@section('font-awesome')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+        integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+@endsection
+
 @section('content')
     <div class="container mx-auto mb-5">
         {{-- * Buttons Container --}}
-        <div class="container mt-3 mb-3">
+        <div class="container d-flex justify-content-end mt-3 mb-3">
             <a href="{{ route('admin.dishes.index') }}" class="btn btn-danger mx-2">
                 Torna alla tabella
             </a>
 
             <a href="{{ route('admin.dishes.edit', $dish) }}" class="btn btn-success">
-                Edita 🖊
+                Modifica  <i class="fa-solid fa-pen-to-square"></i>
             </a>
         </div>
         {{-- * DISH CARD --}}
         <div>
-            <h3>🍲Il mio piatto</h3>
+            <h3>Il mio piatto</h3>
         </div>
-        <div class="card" style="width: 20rem;">
+        <div class="card" style="width: 30rem;">
             <img src="{{ asset($dish->image) }}" class="img-thumbnail" alt="" style="">
             <div class="card-body">
                 <h5 class="card-title">{{ $dish->name }}</h5>
