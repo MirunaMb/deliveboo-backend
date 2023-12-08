e2fda92b442d705299a50365c81742c000e942fb
luigitarallo <tarallo.gigi@gmail.com>
2023-12-08 12:26:08 +0100

    feat: add new card header

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -8,7 +8,7 @@
     <div class="row justify-content-center">
         <div class="col">
             <div class="card">
-                <div class="card-header">{{ __('User Dashboard') }}</div>
+                <div class="card-header">Utente registrato correttamente</div>
 
                 <div class="card-body">
                     @if (session('status'))
