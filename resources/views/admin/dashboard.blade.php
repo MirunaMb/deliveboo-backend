dc940ef7d9a15b328b6f1f1456e6047067ea3b86
luigitarallo <tarallo.gigi@gmail.com>
2023-12-08 19:56:01 +0100

    feat: add italian name

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -2,15 +2,15 @@
 
 @section('content')
 <div class="container">
-    <h2 class="fs-4 text-secondary my-4">
+    {{-- <h2 class="fs-4 text-secondary my-4">
         {{ __('Dashboard') }}
-    </h2>
-    <div class="row justify-content-center">
+    </h2> --}}
+    <div class="row justify-content-center mt-5">
         <div class="col">
             <div class="card">
-                <div class="card-header">Utente registrato correttamente</div>
+                <div class="card-header text-bg-danger text-center fs-3">Sei autenticato</div>
 
-                <div class="card-body">
+                <div class="card-body text-center fs-4">
                     @if (session('status'))
                     <div class="alert alert-success" role="alert">
                         {{ session('status') }}
