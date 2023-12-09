5408c4b1ca91fd869cfc2f509dddf12cb3bc1f12
luigitarallo <tarallo.gigi@gmail.com>
2023-12-09 23:49:31 +0100

    style: add plate-card to order show

--- resources/views/admin/orders/show.blade.php
+++ resources/views/admin/orders/show.blade.php
@@ -1,14 +1,3 @@
-{{-- @extends('layouts.app')
-
-@section('content')
-    <div class="container">
-       
-            <div>
-                {{ $order->guest_name }}
-            </div>
-
-    </div>
-@endsection --}}
 @extends('layouts.app')
 
 @section('content')
@@ -20,32 +9,30 @@
             </a>
         </div>
         {{-- * Order CARD --}}
-        <div class="card">
-            <div class="card-body">
-                <h5 class="card-title">Ordine</h5>
+        <div class="plate-card container p-4">
+            <div class="row">
+                <div class="col-12">
+                    <h4 class="p-3 bg-danger rounded text-light">Ordine numero: {{$order->id}} </h4>
+                    <div class="m-2"><strong>Nome cliente: </strong>{{ $order->guest_name }}</div>
+                    <div class="m-2" ><strong>Cognome : </strong>{{ $order->guest_surname }}</div>
+                    <div class="m-2"><strong>Telefono: </strong>{{ $order->guest_phone }}</div>
+                    <div class="m-2"><strong>E-Mail: </strong>{{ $order->guest_mail }} </div>
+                    <div class="m-2"><strong>Indirizzo: </strong>{{ $order->guest_address }}</div>
+                </div>
+                <div class="col badge text-bg-secondary fs-5 mx-3">
+                    <div class="me-2"><strong>Importo: </strong>€ {{ $order->totalItem }}</div>
+                    <div class="me-2"><strong>Creato il: </strong>{{ $order->created_at->formatLocalized('%e/%m/%Y') }}</div>
+                </div>
             </div>
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">
-                    <span class="me-2">Nome: {{ $order->guest_name }}</span>
-                    <span class="me-2" >Cognome: {{ $order->guest_surname }}</span>
-                    <span class="me-2">Telefono: {{ $order->guest_phone }}</span>
-                    <span class="me-2">E-Mail:{{ $order->guest_mail }} </span>
-                </li>
-                <li class="list-group-item"><strong>Indirizzo: </strong>{{ $order->guest_address }}</li>
-                <li class="list-group-item"></li>
-                <li class="list-group-item"><strong>E-Mail: </strong>{{ $order->guest_mail }}</li>
-                <li class="list-group-item"><strong>Importo: </strong>€ {{ $order->totalItem }}</li>
-                <li class="list-group-item"><strong>Creato il: </strong>{{ $order->created_at->formatLocalized('%e/%m/%Y') }}</li>
-                <ul>
+            <div class="col-7 ms-3">
+                <h4 class="p-3 bg-danger text-light rounded">Prodotti Acquistati</h4>
                     @foreach ($dishes as $dish)
-            <li>{{ $dish->name }} - Quantità: {{ $dish->pivot->quantity }}
-                <img src="{{ $dish->image }}" alt="">
-            </li>
-        @endforeach
-                </ul>
-                    
-              
-            </ul>
+                    <div class="m-2">
+                        <strong>{{$dish->name }}:</strong>
+                         {{ $dish->pivot->quantity }}pz.
+                    </div>
+                    @endforeach
+            </div>
         </div>
     </div>
 @endsection
