be7df5b3c25b18b2c849b81a231f29733eff1b48
JordiMasci <133970995+JordiMasci@users.noreply.github.com>
2023-12-09 18:16:29 +0100

    feat: add seeder users/restaurants/dishes

--- database/seeders/DishSeeder.php
+++ database/seeders/DishSeeder.php
@@ -2,10 +2,11 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Seeder;
-
 use App\Models\Dish;
+use App\Models\Restaurant;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class DishSeeder extends Seeder
 {
@@ -16,22 +17,48 @@ class DishSeeder extends Seeder
      */
     public function run()
     {
-        // $_dishes = [
-        //     ['name' => 'ristorante_1', 'description' => 'Linguaggio di markup strutturale.', 'visible' => true, 'image' => '', 'price' => 12.40],
-        //     ['name' => 'ristorante_2', 'description' => 'Foglio di stile per presentazione.', 'visible' => true, 'image' => '', 'price' => 20.21],
-        //     ['name' => 'ristorante_3', 'description' => 'Linguaggio di scripting client-side.', 'visible' => true, 'image' => '', 'price' => 30.00],
-        //     ['name' => 'ristorante_4', 'description' => 'Framework JavaScript per interfacce utente.', 'visible' => true, 'image' => '', 'price' => 12.93],
-        //     ['name' => 'ristorante_5', 'description' => 'Linguaggio di scripting server-side.', 'visible' => true, 'image' => '', 'price' => 7.50],
-        // ];
+        // piatti Francesi
+        $_dishes = [
+            ['name' => 'Croissant', 'description' => 'Delizioso dolce a base di pasta sfoglia.', 'visible' => true, 'image' => '', 'price' => 3.50, 'restaurant_id' => 1],
+            ['name' => 'Cuisses de grenouilles', 'description' => 'Prelibatezze di rana in salsa aromatica.', 'visible' => true, 'image' => '', 'price' => 18.90, 'restaurant_id' => 1],
+            ['name' => 'galettes bretonnes', 'description' => 'Crepes croccanti con ripieno tradizionale.', 'visible' => true, 'image' => '', 'price' => 12.75, 'restaurant_id' => 1],
+            ['name' => 'Soupe de l\'ognion', 'description' => 'Zuppa di cipolla gratinata e saporita.', 'visible' => true, 'image' => '', 'price' => 9.50, 'restaurant_id' => 1],
+            ['name' => 'ratatouille', 'description' => 'Stufato di verdure tipico della Provenza.', 'visible' => true, 'image' => '', 'price' => 15.20, 'restaurant_id' => 1],
 
-        // foreach ($_dishes as $_dish) {
-        //     $dish = new Dish();
-        //     $dish->name = $_dish['name'];
-        //     $dish->description = $_dish['description'];
-        //     $dish->visible = $_dish['visible'];
-        //     $dish->image = $_dish['image'];
-        //     $dish->price = $_dish['price'];
-        //     $dish->save();
-        // }
+            // piatti Americani
+            ['name' => 'Big Krusty Burgher', 'description' => 'Enorme hamburger con condimenti speciali.', 'visible' => true, 'image' => '', 'price' => 14.99, 'restaurant_id' => 2],
+            ['name' => 'Maccarony and Cheese', 'description' => 'Pasta al formaggio cremosa e irresistibile.', 'visible' => true, 'image' => '', 'price' => 10.50, 'restaurant_id' => 2],
+            ['name' => 'Alette di pollo fritte', 'description' => 'Deliziose ali di pollo croccanti.', 'visible' => true, 'image' => '', 'price' => 18.75, 'restaurant_id' => 2],
+            ['name' => 'Hot-dog', 'description' => 'Panino con salsiccia e condimenti vari.', 'visible' => true, 'image' => '', 'price' => 8.99, 'restaurant_id' => 2],
+            ['name' => 'Patatine Fritte', 'description' => 'Patatine croccanti e deliziose.', 'visible' => true, 'image' => '', 'price' => 5.50, 'restaurant_id' => 2],
+
+            // piatti Italiani
+            ['name' => 'Pizza Margherita', 'description' => 'Pizza classica con pomodoro e mozzarella.', 'visible' => true, 'image' => '', 'price' => 8.50, 'restaurant_id' => 3],
+            ['name' => 'Pizza Mari e Monti', 'description' => 'Pizza con mare e montagna, sapori contrastanti.', 'visible' => true, 'image' => '', 'price' => 12.20, 'restaurant_id' => 3],
+            ['name' => 'Pizza Piccantella', 'description' => 'Pizza piccante con ingredienti gustosi.', 'visible' => true, 'image' => '', 'price' => 10.99, 'restaurant_id' => 3],
+            ['name' => 'Pizza Rucoletta', 'description' => 'Pizza con rucola fresca e ingredienti di qualità.', 'visible' => true, 'image' => '', 'price' => 14.75, 'restaurant_id' => 3],
+            ['name' => 'Pizza Capricciosa', 'description' => 'Pizza ricca e variegata, un vero capriccio.', 'visible' => true, 'image' => '', 'price' => 11.50, 'restaurant_id' => 3],
+
+            // piatti Inglesi
+            ['name' => 'Bangers and Mash (salsicce)', 'description' => 'Salsicce con purè di patate cremoso.', 'visible' => true, 'image' => '', 'price' => 9.75, 'restaurant_id' => 4],
+            ['name' => 'Fish & Chips', 'description' => 'Pesce fritto con patatine, un classico britannico.', 'visible' => true, 'image' => '', 'price' => 13.45, 'restaurant_id' => 4],
+            ['name' => 'Sformato di Carne', 'description' => 'Delizioso sformato di carne.', 'visible' => true, 'image' => '', 'price' => 11.99, 'restaurant_id' => 4],
+            ['name' => 'Uova e pancetta', 'description' => 'Colazione inglese con uova e pancetta.', 'visible' => true, 'image' => '', 'price' => 8.90, 'restaurant_id' => 4],
+            ['name' => 'Yorkshire pudding', 'description' => 'Piatto tradizionale inglese, base di pasta cotta al forno.', 'visible' => true, 'image' => '', 'price' => 6.25, 'restaurant_id' => 4],
+        ];
+
+        foreach ($_dishes as $_dish) {
+            $dish = new Dish();
+            $dish->name = $_dish['name'];
+            $dish->description = $_dish['description'];
+            $dish->visible = $_dish['visible'];
+            $dish->image = $_dish['image'];
+            $dish->price = $_dish['price'];
+
+            $restaurant = Restaurant::find($_dish['restaurant_id']);
+            $restaurant->dishes()->save($dish);
+        }
+
     }
 }
+
--- database/seeders/RestaurantSeeder.php
+++ database/seeders/RestaurantSeeder.php
@@ -17,27 +17,29 @@ class RestaurantSeeder extends Seeder
      */
     public function run()
     {
-        // $_restaurants = [
-        //     ['name' => 'ristorante_1', 'description' => 'Linguaggio di markup strutturale.', 'address' => 'via vatte a pesca', 'phone_number' => '333333333333', 'vat' => '86334519757', 'image' => '', 'user_id' => 1],
-        //     ['name' => 'ristorante_2', 'description' => 'Foglio di stile per presentazione.', 'address' => 'via vatte a pesca', 'phone_number' => '333333333333', 'vat' => '96334519757', 'image' => '', 'user_id' => 2],
-        //     ['name' => 'ristorante_3', 'description' => 'Linguaggio di scripting client-side.', 'address' => 'via vatte a pesca', 'phone_number' => '333333333333', 'vat' => '76334519757', 'image' => '', 'user_id' => 3],
-        //     ['name' => 'ristorante_4', 'description' => 'Framework JavaScript per interfacce utente.', 'address' => 'via vatte a pesca', 'phone_number' => '333333333333', 'vat' => '66334519757', 'image' => '', 'user_id' => 4],
-        //     ['name' => 'ristorante_5', 'description' => 'Linguaggio di scripting server-side.', 'address' => 'via vatte a pesca', 'phone_number' => '333333333333', 'vat' => '56334519757', 'image' => '', 'user_id' => 5],
-        // ];
+        $_restaurants = [
+            ['name' => 'La Ratatouille', 'description' => 'Benvenuti al ristorante del piccolo famoso chef di talento Remy, vincitore di 3 stelle micheline!', 'address' => '168 Rue Montmartre', 'phone_number' => '3310975835', 'vat' => '93849385743', 'image' => '', 'user_id' => 1],
+            ['name' => 'Krusty Burgher', 'description' => 'Dieta? Salute? Acqua passata ! Dacci dentro da Krusty Burgher!', 'address' => '742 Evergreen Terrace', 'phone_number' => '5555552345', 'vat' => '92837485943', 'image' => '', 'user_id' => 2],
+            ['name' => 'U\'PIzzaiuole', 'description' => 'Antica Pizzeria Napoletana dal 1890, benvenuti nella dei Pizzaiuole!', 'address' => 'Via Regina Margherita 28', 'phone_number' => '3243334859', 'vat' => '34959859440', 'image' => '', 'user_id' => 3],
+            ['name' => 'Il Paiolo Magico', 'description' => 'Siete maghi o babbani ? poco importa se vi piace la zuppa calda! Benvenuti al Paiolo Magico!', 'address' => '5d Stoke Newington Rd', 'phone_number' => '4402030484', 'vat' => '83948375484', 'image' => '', 'user_id' => 4],
+
+        ];
 
 
 
-        // foreach ($_restaurants as $_restaurant) {
-        //     $restaurant = new Restaurant();
-        //     $restaurant->name = $_restaurant['name'];
-        //     $restaurant->description = $_restaurant['description'];
-        //     $restaurant->address = $_restaurant['address'];
-        //     $restaurant->phone_number = $_restaurant['phone_number'];
-        //     $restaurant->vat = $_restaurant['vat'];
-        //     $restaurant->image = $_restaurant['image'];
+        foreach ($_restaurants as $_restaurant) {
+            $restaurant = new Restaurant();
+            $restaurant->name = $_restaurant['name'];
+            $restaurant->description = $_restaurant['description'];
+            $restaurant->address = $_restaurant['address'];
+            $restaurant->phone_number = $_restaurant['phone_number'];
+            $restaurant->vat = $_restaurant['vat'];
+            $restaurant->image = $_restaurant['image'];
 
-        //     $user = User::find($_restaurant['user_id']);
-        //     $user->restaurant()->save($restaurant);
-        // }
+            $user = User::find($_restaurant['user_id']);
+            $user->restaurant()->save($restaurant);
+        }
     }
 }
+
+
