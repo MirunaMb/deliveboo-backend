eb2904c08caab6ff7b22334a3afe2302dbcd92b3
LauraDem <laura93dem@gmail.com>
2023-12-09 12:06:30 +0100

    table wrapper

--- resources/views/admin/dishes/index.blade.php
+++ resources/views/admin/dishes/index.blade.php
@@ -8,7 +8,7 @@
 
 @section('content')
 <div class="container p-5">
-<div class="table-wrapper p-5">
+<div class="table-wrapper-2 p-5">
             <h1 class="dishes-title">I miei piatti</h1>
         <table class="fl-table">
             <thead>
