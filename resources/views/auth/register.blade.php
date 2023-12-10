ddc39cc637f69462329b2596d4949045bf5082a2
luigitarallo <tarallo.gigi@gmail.com>
2023-12-10 17:03:12 +0100

    add surname in registration form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -16,7 +16,7 @@
                         @csrf
 
                         <div class="mb-4 row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Nome') }} *</label>
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Nome e Cognome') }} *</label>
 
                             <div class="col-md-6">
                                 <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
